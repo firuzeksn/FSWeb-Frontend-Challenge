c7d332c7f24a1a9be2ecf6aca02ea8958159e0a6
firuzeksn <firuzekusin@gmail.com>
2026-02-09 23:48:16 +0300

    tailwindcss

--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: 'class', // Bu satır kritik!
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // Karanlık modun class ile çalışması için şart
   theme: {
     extend: {
       colors: {
+        // Tasarımdaki özel renkleri buraya isimle tanımlıyoruz
         'brand-purple': '#4731D3',
+        'brand-green': '#CBF281',
         'dark-bg': '#252128',
         'dark-text': '#AEB1FF',
       },
